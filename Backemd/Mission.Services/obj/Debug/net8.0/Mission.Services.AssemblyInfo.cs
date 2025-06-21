ca121533b3eb6f3fb1dfaa1bd3e3e7c414d02ffa
ayan-x1 <pathanayan8347@gmail.com>
2025-06-21 22:02:00 +0530

    intial commit

--- Backemd/Mission.Services/obj/Debug/net8.0/Mission.Services.AssemblyInfo.cs
+++ Backemd/Mission.Services/obj/Debug/net8.0/Mission.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be5f479c3fbc2b4d386eb3eb9c8f5672b19a1cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
